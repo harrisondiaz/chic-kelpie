bda4da19cfb250191827aa0f37dc81752755e17d
Harrison Diaz <harryjhoi01@gmail.com>
2024-03-25 18:58:11 -0500

    Update astro.config.mjs to use Astro's node adapter in standalone mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/static";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: node({
+    mode: "standalone"
+  }),
   integrations: [tailwind()]
 });
