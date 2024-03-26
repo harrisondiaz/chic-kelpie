ce540a455eeb3be3601b71ac93a383aab9e973b3
Harrison Diaz <harryjhoi01@gmail.com>
2024-03-25 19:05:49 -0500

    Add netlify adapter to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
-
 import node from "@astrojs/node";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
+  adapter: netlify(),
   integrations: [tailwind()]
 });
